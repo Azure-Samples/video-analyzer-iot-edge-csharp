e756df443a56a6fa2a9dc5c554ffaabe6c9d4bbd
Keith Hill <Keith@microsoft.com>
2021-05-19 17:45:38 -0500

    JWTTokenIssuer Initial Commit (#13)
    * JWTTokenIssuer Initial Commit
    
    * Update readme.md
    
    * Update readme.md
    
    Co-authored-by: Naiteek Sangani <42254991+naiteeks@users.noreply.github.com>

---
+++ src/jwt-token-issuer/KeyInfo.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft Corporation">
+//      Copyright (C) Microsoft Corporation. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace Tools.JwtTokenIssuer
+{
+    /// <summary>
+    /// Key information.
+    /// </summary>
+    public class KeyInfo
+    {
+        /// <summary>
+        /// Gets or sets the key type.
+        /// </summary>
+        public string KeyType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key algorithm.
+        /// </summary>
+        public string KeyAlgorithm { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key id.
+        /// </summary>
+        public string KeyId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key modulus.
+        /// </summary>
+        public string Modulus { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exponent.
+        /// </summary>
+        public string Exponent { get; set; }
+    }
+}
