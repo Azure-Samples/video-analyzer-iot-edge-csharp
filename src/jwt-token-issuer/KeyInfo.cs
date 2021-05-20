f996ffc4cbb10c1f0c5f89e7575dfbbe77cb157a
Keith Hill <Keith@microsoft.com>
2021-05-19 20:27:26 -0500

    Updating files
    Removal of Newtonsoft.json -- un needed.

--- src/jwt-token-issuer/KeyInfo.cs
+++ src/jwt-token-issuer/KeyInfo.cs
@@ -4,8 +4,6 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
-using Newtonsoft.Json;
-
 namespace Tools.JwtTokenIssuer
 {
     /// <summary>
--- src/jwt-token-issuer/Program.cs
+++ src/jwt-token-issuer/Program.cs
@@ -16,7 +16,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
 
 namespace Tools.JwtTokenIssuer
 {
